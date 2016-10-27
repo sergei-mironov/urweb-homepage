06443a35c7f87eb3ca07e39640575e187e90c86e
Sergey Mironov <grrwlf@gmail.com>
2016-10-28 00:20:02 +0300

    Remove autogen

--- autogen/Slides_Minsk_2014_pdf.ur
+++
@@ -1,6 +0,0 @@
-open Slides_Minsk_2014_pdf_c
-fun content {} = b <- blob () ; returnBlob b (blessMime "application/pdf")
-val propagated_urls : list url = 
-    []
-val url = url(content {})
-val geturl = url
--- autogen/Slides_Minsk_2014_pdf_c.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <urweb.h>
-uw_Basis_blob uw_Slides_Minsk_2014_pdf_c_blob (uw_context ctx, uw_unit unit);
-uw_Basis_string uw_Slides_Minsk_2014_pdf_c_text (uw_context ctx, uw_unit unit);
