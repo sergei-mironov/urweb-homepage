dbb85c81066c07b77c44380a2e33629658a59669
Sergey Mironov <grrwlf@gmail.com>
2016-11-13 15:19:45 +0000

    RM checkmail.sh

--- checkmail.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-swaks "-s" "smtp.gmail.com"  "-p" "587" "-t" ierton@gmail.com "-f" grrwlf@gmail.com "--header" "Subject: ccc" "-S" "--protocol" "ESMTP" "-tls" "-a" "-au" "ierton@gmail.com" "-ap" "12Usvtqkbot" "--body" "test" "--timeout" "5"
-
